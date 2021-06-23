5fe1275b91ccd6194437059a2ece97940516d2d1
aoqing <aoqing1988@yeah.net>
2021-06-23 17:55:01 +0800

    update to 4.3.3

--- aspnet-core/src/Acme.Tarot.Application/TarotApplicationModule.cs
+++ aspnet-core/src/Acme.Tarot.Application/TarotApplicationModule.cs
@@ -5,6 +5,9 @@ using Volo.Abp.Identity;
 using Volo.Abp.Modularity;
 using Volo.Abp.PermissionManagement;
 using Volo.Abp.TenantManagement;
+// using EasyAbp.Abp.TencentCloud.COS;
+// using EasyAbp.Abp.TencentCloud.Common;
+using Microsoft.Extensions.DependencyInjection;
 
 namespace Acme.Tarot
 {
@@ -16,15 +19,37 @@ namespace Acme.Tarot
         typeof(AbpPermissionManagementApplicationModule),
         typeof(AbpTenantManagementApplicationModule),
         typeof(AbpFeatureManagementApplicationModule)
+        // typeof(AbpTencentCloudCommonModule),
+        // typeof(AbpTencentCloudCOSModule)
         )]
     public class TarotApplicationModule : AbpModule
     {
         public override void ConfigureServices(ServiceConfigurationContext context)
         {
+            var configuration = context.Services.GetConfiguration();
+
+            // const appId = configuration[""]
+
+
             Configure<AbpAutoMapperOptions>(options =>
             {
                 options.AddMaps<TarotApplicationModule>();
             });
+
+            // Configure<AbpTencentCloudCommonOptions>(options =>
+            // {
+            //     options.AppId = "1259787355";
+            //     options.SecretId = "AKIDxlKcdNr51g6FaeS0IJ3WidHekZ7PQAeq";
+            //     options.SecretKey = "xd2A1buwiESP5VeW7ankQs4ze3B2iBNJ";
+            // });
+
+            // Configure<AbpTencentCloudCOSOptions>(options =>
+            // {
+            //     options.Region = "ap-chengdu";
+            //     options.ConnectionTimeout = 1000 * 60 * 15;
+            //     options.KeyDurationSecond = 1000 * 60 * 60;
+            //     options.ReadWriteTimeout = 1000 * 60 * 60;
+            // });
         }
     }
 }
